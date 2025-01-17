18c0279fbc94b80a71dbb01e2a54bb1f99469b49
mounikagollu1996 <mounikagollu1996@gmail.com>
2025-01-17 18:09:02 +0530

    add skeleton UI

--- react-calculator-tdd/src/App.js
+++ react-calculator-tdd/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-container">
+      <div className="calculator-container">
+        <input type="text" value="" readOnly className=""/>
+        <div>result:</div>
+        <div className="buttons-container">
+          {[1, 2, 3, "+", 4, 5, 6, "-", 7, 8, 9, "*", 0, "C", "="].map(
+            (btn) => (
+              <button>{btn}</button>
+            )
+          )}
+        </div>
+      </div>
     </div>
   );
 }
