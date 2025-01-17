8db07d5e05593b87f66f4962bcebad4586a38d4e
mounikagollu1996 <mounikagollu1996@gmail.com>
2025-01-17 19:41:10 +0530

    add addition functionality

--- react-calculator-tdd/src/App.js
+++ react-calculator-tdd/src/App.js
@@ -1,22 +1,37 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [input,setInput] = useState("");
+  const [input, setInput] = useState("");
+  const [result, setResult] = useState("");
 
   const handleButtonClick = (value) => {
     setInput((prev) => prev + value);
   };
-  
+
+  const calculateResult = () => {
+    const result = eval(input);
+    setResult(result);
+  };
+
   return (
     <div className="main-container">
       <div className="calculator-container">
-        <input type="text" value={input} readOnly className=""/>
-        <div>Result:</div>
+        <input type="text" value={input} readOnly className="" />
+        <div>Result: {result}</div>
         <div className="buttons-container">
           {[1, 2, 3, "+", 4, 5, 6, "-", 7, 8, 9, "*", 0, "C", "="].map(
-            (btn) => (
-              <button onClick={()=>handleButtonClick(btn.toString())}>{btn}</button>
+            (value, index) => (
+              <button
+                key={index}
+                onClick={
+                  value === "="
+                    ? calculateResult
+                    : () => handleButtonClick(value.toString())
+                }
+              >
+                {value}
+              </button>
             )
           )}
         </div>
